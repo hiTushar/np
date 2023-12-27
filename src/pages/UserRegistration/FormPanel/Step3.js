40d51deafb601dad46d69476909e3b00dddd7a1a
tusharMF <tushar.jain@monsoonfish.com>
2023-12-27 17:28:38 +0530

    Refactor loader code

--- src/pages/UserRegistration/FormPanel/Step3.js
+++ src/pages/UserRegistration/FormPanel/Step3.js
@@ -1,21 +1,21 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import ProgressBar from '../../../components/ProgressBar/ProgressBar';
 import activation_success from '../assets/activation_success.svg';
 
-let timer = null;
 export default function Step3() {
     const [progress, setProgress] = useState(0);
+    const timerId = useRef(null);
 
     useEffect(() => {
-        if(!timer) {
-            timer = setInterval(() => {
+        if(!timerId.current) {
+            timerId.current = setInterval(() => {
                 console.log('*')
                 setProgress(prev => prev + .25);
             }, 10)
         }
         return () => {
             if(progress >= 100) {
-                clearInterval(timer);
+                clearInterval(timerId.current);
             }
         }
     }, [progress])
