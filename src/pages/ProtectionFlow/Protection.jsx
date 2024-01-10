93f03698db767995671afa6a2cac51b01fcd8f7b
adnan-monsoonfish <adnan.khan@monsoonfish.com>
2024-01-10 11:28:31 +0530

    added font family and needed svgs

--- src/pages/ProtectionFlow/Protection.jsx
+++ src/pages/ProtectionFlow/Protection.jsx
@@ -1,26 +1,32 @@
 import React from 'react'
 import "./protection.scss"
-import { ReactComponent as NPAVSVG } from "../../assets/npav.svg";
-import { ReactComponent as HEADPHONESVG } from "../../assets/headphone.svg";
-import { ReactComponent as SETTINGSSVG } from "../../assets/settings.svg";
-import { ReactComponent as USERSVG } from "../../assets/user.svg";
+import NpavSVG from '../../components/SvgComponents/NpavSVG';
+import HeadPhoneSVG from '../../components/SvgComponents/HeadPhoneSVG';
+import SettingsSVG from '../../components/SvgComponents/SettingsSVG';
+import UserSVG from '../../components/SvgComponents/UserSVG';
+import ArrowSVG from '../../components/SvgComponents/ArrowSVG';
 
 const Protection = () => {
   return (
     <div className="protection_main_container">
       <div className="protection_header">
         <div className='npav__svg'>
-          <NPAVSVG />
+          <NpavSVG />
         </div>
         <div className='heading_txt'>Net Protector Antivirus 2025</div>
         <div className='header_ctas'>
-          <HEADPHONESVG/>
-          <SETTINGSSVG/>
-          <USERSVG/>
+          <HeadPhoneSVG/>
+          <SettingsSVG/>
+          <UserSVG/>
         </div>
       </div>
       <div className="protection_child_container">
-
+        <div>
+        <ArrowSVG/>
+        </div>
+        <div>
+        Protection Type
+        </div>
       </div>
     </div>
   )
