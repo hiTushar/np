385db55fb7124181acda3fe659917917214dd748
adnan-monsoonfish <adnan.khan@monsoonfish.com>
2024-01-12 15:58:17 +0530

    implemented functional part and will do merging of screen 2

--- src/components/commons/SideBar/SideBar.jsx
+++ src/components/commons/SideBar/SideBar.jsx
@@ -11,6 +11,8 @@ import MailOutlined from './SVGComponents/MailOutlined'
 import PartitionOutlined from './SVGComponents/PartitionOutlined'
 import ThunderBoltOutlined from './SVGComponents/ThunderBoltOutlined'
 import UnGroupOutlined from './SVGComponents/UnGroupOutlined'
+import { useDispatch, useSelector } from 'react-redux'
+import { setProtectionEnabled, setProtectionSelected } from '../../../redux/actions/protectionFeatureActions'
 
 
 const sideBarDataArr = [
@@ -18,76 +20,89 @@ const sideBarDataArr = [
         SVGComponent: AppStoreOutlined,
         itemNm: "Application Control",
         enabled: false,
-        selected:false,
+        selected: false,
     },
     {
         SVGComponent: GoldOutlined,
         itemNm: "ZV Fort Protection",
         enabled: false,
-        selected:false,
+        selected: false,
     },
     {
         SVGComponent: CloudOutlined,
         itemNm: "Cloud Protection",
         enabled: true,
-        selected:true,
+        selected: true,
     },
     {
         SVGComponent: DownCircleOutlined,
         itemNm: "OS Protection",
         enabled: false,
-        selected:false,
+        selected: false,
     },
     {
         SVGComponent: StopOutlined,
         itemNm: "Advertise Blocker",
         enabled: false,
-        selected:false,
+        selected: false,
     },
     {
         SVGComponent: SecurityScanOutlined,
         itemNm: "Online Protection",
         enabled: false,
-        selected:false,
+        selected: false,
     },
     {
         SVGComponent: MailOutlined,
         itemNm: "Email Protection",
         enabled: true,
-        selected:false,
+        selected: false,
     },
     {
         SVGComponent: PartitionOutlined,
         itemNm: "LAN Protection",
         enabled: false,
-        selected:false,
+        selected: false,
     },
     {
         SVGComponent: ThunderBoltOutlined,
         itemNm: "IDS",
         enabled: false,
-        selected:false,
+        selected: false,
     },
     {
         SVGComponent: UnGroupOutlined,
         itemNm: "Data Protection & Backup",
         enabled: false,
-        selected:false,
+        selected: false,
     }
 ]
 
 const SideBar = () => {
+    const { selected, enabled:allEnabled } = useSelector(state => state.protectionSelReducer.allData);
+    const dispatch = useDispatch()
+    const sideBarFeatureSelectionHandler = (event, idx) => {
+        event.preventDefault();
+        dispatch(setProtectionSelected(idx))
+    }
+    const switchToggleHandler = (event,idx) => {
+        event.preventDefault();
+        dispatch(setProtectionEnabled(idx))
+
+    }
     return (
         <div className='sidebar__main_container'>
             {sideBarDataArr.map((item, idx) => {
-                const { SVGComponent, itemNm, enabled ,selected} = item
-                return (<div key={idx} className={`sec_left_item ${selected && "enabled"}`}>
+                const { SVGComponent, itemNm } = item
+                const isSelected = selected === idx;
+                const isEnabled = allEnabled[idx]
+                return (<div onClick={(event) => sideBarFeatureSelectionHandler(event, idx)} key={idx} className={`sec_left_item ${isSelected && "enabled"}`}>
                     <div className={`sec_left_item_lftgrp `}>
-                        <SVGComponent custFill={selected ? "#C3E3E9" : "#676A84"} />
-                        <div className={`sec_left_item_text ${selected && "enabled"}`}>{itemNm}</div>
+                        <SVGComponent custFill={isSelected ? "#C3E3E9" : "#676A84"} />
+                        <div className={`sec_left_item_text ${isSelected && "enabled"}`}>{itemNm}</div>
                     </div>
-                    {!selected ?
-                        <InputSwitch isToggled={enabled}/> : ""
+                    {!isSelected ?
+                        <InputSwitch onToggle={event =>switchToggleHandler(event,idx) } isToggled={isEnabled} /> : ""
                     }
                 </div>)
             }
---
+++ src/redux/reducers/protectionSelectedFeature.js
@@ -0,0 +1,37 @@
+import { PROTECTION_SELECTED_FEATURE, SET_ENABLED_FEATURES } from "../actions/actionTypes";
+
+const initialState = {
+    allData:{
+        enabled: [false, false, true, false, false, false, true, false, false, false],
+        selected: 2,
+    }
+}
+
+const protectionSelReducer = (state = initialState, action) => {
+    const { type, payload } = action;
+    console.log(action)
+    switch (type) {
+        case SET_ENABLED_FEATURES:
+            let currEnabledArr = [...state.allData.enabled];
+            currEnabledArr[payload.enabledIdx] = !currEnabledArr[payload.enabledIdx]
+            return {
+                ...state,
+                allData:{
+                    enabled:[...currEnabledArr],
+                    selected: state.allData.selected
+                }
+            }
+        case PROTECTION_SELECTED_FEATURE:
+            return {
+                ...state,
+                allData:{
+                    enabled:[...state.allData.enabled],
+                    selected: payload.selected
+                }
+            }
+        default:
+            return state;
+    }
+}
+
+export { protectionSelReducer };
