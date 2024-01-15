3e64ede815d1e3921c367d1ef17babd1fd98d96d
Monsoonfish-Sameer <sameer.chikshe@monsoonfish.com>
2024-01-15 16:47:42 +0530

    protection flow screen 4 dev

--- src/pages/ProtectionFlow/Protection.jsx
+++ src/pages/ProtectionFlow/Protection.jsx
@@ -3,6 +3,7 @@ import "./protection.scss"
 import ProtectionFlowLayout from '../../components/Layout/ProtectionFlowLayout/ProtectionFlowLayout';
 import CloudProtection from '../../components/ProtectionFlowComponents/CloudProtection/CloudProtection';
 import { useSelector } from 'react-redux';
+import DataProtection from '../../components/ProtectionFlowComponents/CloudProtection/DataProtection';
 
 const Protection = () => {
   const { selected, enabled } = useSelector(state => state.protectionSelReducer.allData);
@@ -17,14 +18,21 @@ const Protection = () => {
     CloudProtection,
     CloudProtection,
     CloudProtection,
-    CloudProtection
+    DataProtection,
   ]
 
   return (
     <ProtectionFlowLayout>
       {protectionFlowContentComponentsArr.map(
-        (Component, idx) => <>
-          {idx === selected ? <Component key={idx} selected={enabled[selected]}  enabledIdx={idx}/> : ""}</>
+        (Component, idx) => {
+          let propsObj = { selected: enabled[selected], enabledIdx: idx };
+          if (idx === protectionFlowContentComponentsArr.length - 1) {
+            propsObj["width"] = "12%"
+          }
+
+          return (<>
+            {idx === selected ? <Component key={idx} {...propsObj} /> : ""}</>)
+        }
       )}
     </ProtectionFlowLayout>
   )
