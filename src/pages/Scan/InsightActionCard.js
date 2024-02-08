2975b97b8134b18f9b0bb41b694442196e53265e
tusharMF <tushar.jain@monsoonfish.com>
2024-02-08 17:05:17 +0530

    Refactor code

--- src/pages/Scan/InsightActionCard.js
+++ src/pages/Scan/InsightActionCard.js
@@ -1,21 +1,21 @@
 import Card from "../../components/Card/Card";
 import arrowLeft from '../../assets/arrow-left.png';
 
-export default function InsightActionCard(props) {
-    const { data } = props;
+export default function InsightActionCard({ data }) {
+    const { icon, title, desc, action } = data;
     return (
         <Card>
             <div className='card__title'>
                 <div className='title__icon'>
-                    <img src={data.icon} alt='icon' />
+                    <img src={icon} alt='icon' />
                 </div>
-                <div className='title__name'>{data.title}</div>
+                <div className='title__name'>{title}</div>
             </div>
             <div className='card__desc'>
-                {data.desc}
+                {desc}
             </div>
             <div className='card__action'>
-                <div className='action__title'>{data.action}</div>
+                <div className='action__title'>{action}</div>
                 <div className='action__next'>
                     <img src={arrowLeft} alt='arrow next' />
                 </div>
