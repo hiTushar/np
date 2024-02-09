d4ea3d2793cc8a5bfb934488915387909bf3441f
tusharMF <tushar.jain@monsoonfish.com>
2024-02-09 16:14:05 +0530

    Refactor card UI to make it more generic

--- src/pages/Scan/InsightActionCard.js
+++ src/pages/Scan/InsightActionCard.js
@@ -5,19 +5,21 @@ export default function InsightActionCard({ data }) {
     const { icon, title, desc, action } = data;
     return (
         <Card>
-            <div className='card__title'>
-                <div className='title__icon'>
-                    <img src={icon} alt='icon' />
+            <div className="insight-action-card">
+                <div className='insight-action-card__title'>
+                    <div className='title__icon'>
+                        <img src={icon} alt='icon' />
+                    </div>
+                    <div className='title__name'>{title}</div>
                 </div>
-                <div className='title__name'>{title}</div>
-            </div>
-            <div className='card__desc'>
-                {desc}
-            </div>
-            <div className='card__action'>
-                <div className='action__title'>{action}</div>
-                <div className='action__next'>
-                    <img src={arrowLeft} alt='arrow next' />
+                <div className='insight-action-card__desc'>
+                    {desc}
+                </div>
+                <div className='insight-action-card__action'>
+                    <div className='action__title'>{action}</div>
+                    <div className='action__next'>
+                        <img src={arrowLeft} alt='arrow next' />
+                    </div>
                 </div>
             </div>
         </Card>
