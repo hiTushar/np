71954ac38bcce41aa09a5f5b15f558ac40a08f49
tusharMF <tushar.jain@monsoonfish.com>
2024-02-21 17:49:15 +0530

    UI, layout fix

--- src/pages/UserRegistration/FormPanel/Step1.js
+++ src/pages/UserRegistration/FormPanel/Step1.js
@@ -56,7 +56,63 @@ export default function Step1() {
     }
 
     return (
-        <div className="user-formpanel__step1">
+        <>
+            <div className="user-formpanel__step1">
+                <div className='user-formpanel__step1-body'>
+                    <p>Dear customer!</p>
+
+                    <div className='user-formpanel__step1-key'>
+                        <p>Please enter key number provided inside the box</p>
+                        <Input 
+                            ref={keyRef}
+                            value={key}
+                            placeholder={'X - XXXXXXXXXX'}
+                            maxLength={14}
+                            type={'text'}
+                            onChange={onKeyChange}
+                        />
+                    </div>
+                    <div className='user-formpanel__step1-type'>
+                        <p>Select user type</p>
+                        <Radio 
+                            options={[
+                                {
+                                    name: 'Single User',
+                                    value: 'single'
+                                },
+                                {
+                                    name: 'Multiple PCs with corporate ID',
+                                    value: 'multi'
+                                }
+                            ]}
+                            defaultValue={{
+                                name: 'Single User',
+                                value: 'single'
+                            }}
+                            customStyle={{}}
+                            onChange={onTypeChange}
+                        />
+                    </div>
+                    <div className='user-formpanel__step1-code'>
+                        <p>Your Installation Code</p>
+                        <Input 
+                            value={'NN-55697-45887-33441-42095-07498'}
+                            readOnly={true}
+                            customStyle={{ 
+                                display: 'flex',
+                                justifyContent: 'space-between',
+                                alignItems: 'center'
+                            }}
+                        >
+                            <img src={copyIcon} className={'user-formpanel__step1-code-copy'} alt={'copy icon'} onClick={() => copyCode('NN-55697-45887-33441-42095-07498')} />
+                        </Input>
+                        <p>One key can be used on one system only</p>
+                    </div>
+                </div>
+                <div className='user-formpanel__step1-footer'>
+                    <Button text={'NEXT'} onClick={nextPage} type={'secondary'} />
+                </div>
+            </div>
             <ToastContainer 
                 position="top-center"
                 autoClose={2000}
@@ -69,58 +125,6 @@ export default function Step1() {
                 pauseOnHover
                 theme="dark"
             />
-            <p>Dear customer!</p>
-
-            <div className='user-formpanel__step1-key'>
-                <p>Please enter key number provided inside the box</p>
-                <Input 
-                    ref={keyRef}
-                    value={key}
-                    placeholder={'X - XXXXXXXXXX'}
-                    maxLength={14}
-                    type={'text'}
-                    onChange={onKeyChange}
-                />
-            </div>
-            <div className='user-formpanel__step1-type'>
-                <p>Select user type</p>
-                <Radio 
-                    options={[
-                        {
-                            name: 'Single User',
-                            value: 'single'
-                        },
-                        {
-                            name: 'Multiple PCs with corporate ID',
-                            value: 'multi'
-                        }
-                    ]}
-                    defaultValue={{
-                        name: 'Single User',
-                        value: 'single'
-                    }}
-                    customStyle={{}}
-                    onChange={onTypeChange}
-                />
-            </div>
-            <div className='user-formpanel__step1-code'>
-                <p>Your Installation Code</p>
-                <Input 
-                    value={'NN-55697-45887-33441-42095-07498'}
-                    readOnly={true}
-                    customStyle={{ 
-                        display: 'flex',
-                        justifyContent: 'space-between',
-                        alignItems: 'center'
-                    }}
-                >
-                    <img src={copyIcon} className={'user-formpanel__step1-code-copy'} alt={'copy icon'} onClick={() => copyCode('NN-55697-45887-33441-42095-07498')} />
-                </Input>
-                <p>One key can be used on one system only</p>
-            </div>
-            <div className='user-formpanel__step1-footer'>
-                <Button text={'NEXT'} onClick={nextPage} type={'secondary'} />
-            </div>
-        </div>
+        </>
     )
 }
