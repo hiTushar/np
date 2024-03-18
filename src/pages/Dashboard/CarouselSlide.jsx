7b7a5ac19722e92d84af883c7975fdef37e50546
tusharMF <tushar.jain@monsoonfish.com>
2024-03-18 12:05:28 +0530

    Minor refactor

---
+++ src/pages/Dashboard/CarouselSlide.jsx
@@ -0,0 +1,97 @@
+import './CarouselSlide.css';
+import DailyReport from './DailyReport';
+import ScanSection from './ScanSection';
+import UpgradeCta from './UpgradeCta';
+import UtilityCard from './UtilityCard';
+import { gearCheckPng, globeCheckPng, privacyCheckPng, shieldCheckPng, indicatorInsecure, indicatorPartial, indicatorSecure } from "../../assets/assets";
+
+const UTILITIES_CARD_DATA = [
+    {
+        key: 'protection',
+        title: 'Protection',
+        icon: shieldCheckPng,
+        desc: 'Enable web-shield and safe application mode to increase the score',
+        inc: '20',
+        navUrl: '/user/protection'
+    },
+    {
+        key: 'privacy',
+        title: 'Privacy',
+        icon: privacyCheckPng,
+        desc: 'Enable web-shiled and safe application mode to increase the score',
+        inc: '10',
+        navUrl: '/user/privacy'
+    },
+    {
+        key: 'tuner',
+        title: 'System tuner',
+        icon: gearCheckPng,
+        desc: 'Enable block/unblock games to increase the security score',
+        inc: '25',
+        navUrl: '/user/system-tuner'
+    },
+    {
+        key: 'web',
+        title: 'Web Security',
+        icon: globeCheckPng,
+        desc: 'Enable block/unblock games to increase the security score',
+        inc: '35',
+        navUrl: '/user/web-security'
+    },
+]
+
+const getStatusNotch = (status) => {
+    switch (status) {
+        case 'secure':
+            return indicatorSecure;
+        case 'partial':
+            return indicatorPartial;
+        case 'insecure':
+            return indicatorInsecure;
+        default:
+            return null;
+    }
+}
+
+export default function CarouselSlide ({ dataPt, upgradeStatus, index }) { 
+   return (
+        <div className="" key={dataPt.timestamp}>
+            <div className="slide__scan-section">
+                <ScanSection data={dataPt} index={index} />
+            </div>
+            <div className="slide__report-section">
+                <div className="report-section__title">Daily Report</div>
+                <div className="report-section__panel">
+                    <div className="panel__daily-data">
+                        <DailyReport
+                            system_status={dataPt.system_status}
+                            secure_percent={dataPt.secure_percent}
+                            last_week_percent={dataPt.last_week_percent}
+                            threats_found={dataPt.threats_found}
+                            threats_fixed={dataPt.threats_fixed}
+                            files_scanned={dataPt.files_scanned}
+                        />
+                    </div>
+                    {
+                        upgradeStatus && (
+                            <div className="panel__upgrade-cta">
+                                <UpgradeCta />
+                            </div>
+                        )
+                    }
+                </div>
+            </div>
+            <div className="slide__utilities-section">
+                <div className="utilities-section__title">Utilities</div>
+                <div className="utilities-section__cards">
+                    {
+                        UTILITIES_CARD_DATA.map(cardData => <UtilityCard {...cardData} key={cardData.key} />)
+                    }
+                </div>
+            </div>
+            <div className='slide__indicator'>
+                <img src={getStatusNotch(dataPt.system_status)} alt='panel indicator' style={{ fill: '#FFF' }} />
+            </div>
+        </div>
+    )
+} 
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,47 +1,9 @@
 import './Dashboard.css';
-import UtilityCard from "./UtilityCard";
-import { gearCheckPng, globeCheckPng, indicatorInsecure, indicatorPartial, indicatorSecure, privacyCheckPng, shieldCheckPng } from "../../assets/assets";
-import DailyReport from "./DailyReport";
-import UpgradeCta from "./UpgradeCta";
 import { useSelector } from "react-redux";
 import { ReportData } from './ReportData';
 import { useEffect, useRef, useState } from 'react';
-import ScanSection from './ScanSection';
+import CarouselSlide from './CarouselSlide';
 
-const UTILITIES_CARD_DATA = [
-    {
-        key: 'protection',
-        title: 'Protection',
-        icon: shieldCheckPng,
-        desc: 'Enable web-shield and safe application mode to increase the score',
-        inc: '20',
-        navUrl: '/user/protection'
-    },
-    {
-        key: 'privacy',
-        title: 'Privacy',
-        icon: privacyCheckPng,
-        desc: 'Enable web-shiled and safe application mode to increase the score',
-        inc: '10',
-        navUrl: '/user/privacy'
-    },
-    {
-        key: 'tuner',
-        title: 'System tuner',
-        icon: gearCheckPng,
-        desc: 'Enable block/unblock games to increase the security score',
-        inc: '25',
-        navUrl: '/user/system-tuner'
-    },
-    {
-        key: 'web',
-        title: 'Web Security',
-        icon: globeCheckPng,
-        desc: 'Enable block/unblock games to increase the security score',
-        inc: '35',
-        navUrl: '/user/web-security'
-    },
-]
 
 export default function Dashboard({ props }) {
     const upgradeStatus = useSelector(state => state.commonReducer['upgrade_done']);
@@ -81,23 +43,23 @@ export default function Dashboard({ props }) {
             let allChildNodes = carouselRef.current.children;
             let currentReportIndex = Array.from(carouselRef.current.children).findIndex(node => node.classList.contains('slide__one'));
 
-            if (currentReportIndex > selectedReportIndex) {
+            if (currentReportIndex > selectedReportIndex) { // moving slides backwards
                 let steps = Math.abs(currentReportIndex - selectedReportIndex);
                 for (let i = 0; i < steps; i++) {
                     setTimeout(() => {
                         slideClassArray.current.push('slide__after');
-                        if (slideClassArray.current[0] === 'slide__ahead')
+                        if (slideClassArray.current[0] === 'slide__ahead') // so as to not lose the slide specific positioning classes - slide__one, ...
                             slideClassArray.current.shift();
                         applyClassArrayToSlides(slideClassArray.current, allChildNodes);
                     }, 400 * i)
                 }
             }
-            else if (currentReportIndex < selectedReportIndex) {
+            else if (currentReportIndex < selectedReportIndex) { // moving slides forward
                 let steps = Math.abs(currentReportIndex - selectedReportIndex);
                 for (let i = 0; i < steps; i++) {
                     setTimeout(() => {
                         slideClassArray.current.unshift('slide__ahead');
-                        if (slideClassArray.current[slideClassArray.current.length - 1] === 'slide__after')
+                        if (slideClassArray.current[slideClassArray.current.length - 1] === 'slide__after') // so as to not lose the slide specific positioning classes - slide__one, ...
                             slideClassArray.current.pop();
                         applyClassArrayToSlides(slideClassArray.current, allChildNodes);
                     }, 400 * i)
@@ -113,67 +75,13 @@ export default function Dashboard({ props }) {
         })
     }
 
-    const getStatusNotch = (status) => {
-        switch (status) {
-            case 'secure':
-                return indicatorSecure;
-            case 'partial':
-                return indicatorPartial;
-            case 'insecure':
-                return indicatorInsecure;
-            default:
-                return null;
-        }
-    }
-
-    const CarouselSlide = ({ dataPt, index }) => (
-        <div className="" key={dataPt.timestamp}>
-            <div className="slide__scan-section">
-                <ScanSection data={dataPt} index={index} />
-            </div>
-            <div className="slide__report-section">
-                <div className="report-section__title">Daily Report</div>
-                <div className="report-section__panel">
-                    <div className="panel__daily-data">
-                        <DailyReport
-                            system_status={dataPt.system_status}
-                            secure_percent={dataPt.secure_percent}
-                            last_week_percent={dataPt.last_week_percent}
-                            threats_found={dataPt.threats_found}
-                            threats_fixed={dataPt.threats_fixed}
-                            files_scanned={dataPt.files_scanned}
-                        />
-                    </div>
-                    {
-                        upgradeStatus && (
-                            <div className="panel__upgrade-cta">
-                                <UpgradeCta />
-                            </div>
-                        )
-                    }
-                </div>
-            </div>
-            <div className="slide__utilities-section">
-                <div className="utilities-section__title">Utilities</div>
-                <div className="utilities-section__cards">
-                    {
-                        UTILITIES_CARD_DATA.map(cardData => <UtilityCard {...cardData} key={cardData.key} />)
-                    }
-                </div>
-            </div>
-            <div className='slide__indicator'>
-                <img src={getStatusNotch(dataPt.system_status)} alt='panel indicator' style={{ fill: '#FFF' }} />
-            </div>
-        </div>
-    )
-
     return (
         <div className="dashboard">
             <div className='dashboard__carousel' ref={carouselRef}>
                 {
                     /* separate component for better debugging */
                     allReportPanels.map((dataPt, index) => (
-                        <CarouselSlide key={new Date(dataPt.timestamp)} dataPt={dataPt} index={index} />
+                        <CarouselSlide key={new Date(dataPt.timestamp)} upgradeStatus={upgradeStatus} dataPt={dataPt} index={index} />
                     ))
                 }
             </div>
