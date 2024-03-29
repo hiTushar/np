37bf59840968864ac3e38644d4554b8ee3467768
tusharMF <tushar.jain@monsoonfish.com>
2024-03-29 12:07:26 +0530

    Add Protection settings side panel

--- src/pages/Protection/ProtectionSidePanel.jsx
+++ src/pages/Protection/ProtectionSidePanel.jsx
@@ -1,24 +1,36 @@
 import { useNavigate } from 'react-router-dom';
 import './Protection.css';
+import { useSelector } from 'react-redux';
+import Switch from '../../components/Switch/Switch';
 
 export default function ProtectionSidePanel({ sectionRoute, data }) {
     const navigate = useNavigate();
     const goto = (section) => navigate(`/user/protection?section=${section}`);
-    
-    if(sectionRoute === null) sectionRoute = 'app';
+
+    const { protectionReducer } = useSelector(state => state);
 
     return (
         <div className='protection__side-panel'>
             {
                 data.map(sectionData => {
                     const { key, title, svgIcon } = sectionData;
-                    
+
                     return (
                         <div className={`side-panel__section ${sectionRoute === key ? 'highlight' : ''}`} onClick={() => goto(key)} key={key}>
                             <div className='section__icon'>
                                 {svgIcon(sectionRoute === key ? '#C3E3E9' : '#676A84')}
                             </div>
                             <div className={`section__title ${sectionRoute === key ? 'highlight' : ''}`}>{title}</div>
+                            {
+                                sectionRoute !== key && (
+                                    <div className='section__status'>
+                                        <Switch
+                                            disabled={true}
+                                            checked={protectionReducer[key]}
+                                        />
+                                    </div>
+                                )
+                            }
                         </div>
                     )
                 })
---
+++ src/pages/Protection/routeReduxKeyMap.jsx
@@ -0,0 +1,15 @@
+const routeReduxKeyMap = {
+    app: application_control,
+    fort: zv_fort_protection,
+    cloud: cloud_protection,
+    os: os_protection,
+    adblock: ad_blocker,
+    online: online_protection,
+    email: email_protection,
+    lan: lan_protection,
+    ids: ids,
+    backup: data_backup
+}
+
+export default routeReduxKeyMap;
+// now redundant as route key and redux key are same
