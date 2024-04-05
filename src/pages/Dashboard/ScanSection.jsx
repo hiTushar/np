4ff5499d62dcb85af708182b19abb4d39c7fafe8
tusharMF <tushar.jain@monsoonfish.com>
2024-04-05 12:05:21 +0530

    Bug fix quick scan on the homepage

--- src/pages/Dashboard/ScanSection.jsx
+++ src/pages/Dashboard/ScanSection.jsx
@@ -9,12 +9,12 @@ import { setScanning } from '../../redux/actions/scanStatusActions';
 import ScanProgress from '../../components/ScanProgress/ScanProgress';
 import { splitTimestamp } from '../../utils/Utils';
 
-export default function ScanSection({data, slideIndex}) {
+export default function ScanSection({ data, slideIndex }) {
     const dispatch = useDispatch();
     const scanStatusData = useSelector(state => state.scanStatusReducer);
     const navigate = useNavigate();
     const { system_status, files_scanned, threats_found, threats_fixed, timestamp } = data;
-    
+
     const fileTimerId = useRef(null);
     const timeTimerId = useRef(null);
 
@@ -52,41 +52,59 @@ export default function ScanSection({data, slideIndex}) {
 
     // TODO: make all this timer function either as custom hook or function - IMP
     useEffect(() => {
-        if(scanStatusData.scanning) {
-            if(!fileTimerId.current) {
-                fileTimerId.current = setInterval(() => {
-                    setScannedFileCount(prev => prev + 1);
-                }, 100)
+        if(slideIndex === 0) {
+            if (scanStatusData.scanning) {
+                // if (!fileTimerId.current) {
+                    fileTimerId.current = setInterval(() => {
+                        setScannedFileCount(prev => prev + 1);
+                    }, 100)
+                // }
+    
+                // if (!timeTimerId.current) {
+                    timeTimerId.current = setInterval(() => {
+                        setPanelData(prev => ({ ...prev, time_left: prev.time_left - 10 }))
+                    }, 1)
+                // }
             }
     
-            if(!timeTimerId.current) {
-                timeTimerId.current = setInterval(() => {
-                    setPanelData(prev => ({ ...prev, time_left: prev.time_left - 10}))
-                }, 1)
+            return () => {
+                clearInterval(fileTimerId.current);
+                clearInterval(timeTimerId.current);
             }
         }
     }, [scanStatusData.scanning])
 
     useEffect(() => {
-        if(scanStatusData.scanning) {
-            if(progress >= 1) {
-                clearInterval(fileTimerId.current);
-            }
-            if(progress >= 1 || panelData.time_left <= 0) {
-                clearInterval(timeTimerId.current);
+        if(slideIndex === 0) {
+            if (scanStatusData.scanning) {
+                // console.log(progress, scannedFileCount, panelData.time_left);
+                if (progress >= 1 || panelData.time_left <= 0) {
+                    clearInterval(timeTimerId.current);
+                    clearInterval(fileTimerId.current);
+                    setTimeout(() => {
+                        setProgress(0);
+                        setScannedFileCount(0);
+                        setPanelData(prev => ({ ...prev, time_left: 90000 }));
+                        dispatch(setScanning(false));
+                    }, 2000)
+                }
             }
         }
     }, [progress, panelData.time_left])
 
     useEffect(() => {
-        setProgress(scannedFileCount / totalFileCount);
+        if(slideIndex === 0) {
+            setProgress(scannedFileCount / totalFileCount);
+        }
     }, [scannedFileCount, totalFileCount])
 
     const scanSwitch = () => {
-        if(!scanPause) {
+        if (!scanPause) {
             setScanPause(true);
             clearInterval(fileTimerId.current);
+            // fileTimerId.current = null;
             clearInterval(timeTimerId.current);
+            // timeTimerId.current = null;
         }
         else {
             setScanPause(false);
@@ -96,37 +114,38 @@ export default function ScanSection({data, slideIndex}) {
             }, 100)
 
             timeTimerId.current = setInterval(() => {
-                setPanelData(prev => ({ ...prev, time_left: prev.time_left - 10}))
+                setPanelData(prev => ({ ...prev, time_left: prev.time_left - 10 }))
             }, 1)
         }
     }
 
     // TODO: create utility function to create pluralized text based on the count
     const getThreatCountText = (threats_found) => {
-        if(parseInt(threats_found) !== 1) {
+        if (parseInt(threats_found) !== 1) {
             return `${threats_found} threats found`;
         }
         return `${threats_found} threat found`;
     }
 
     const getScannedFilesText = (files_scanned) => {
-        if(parseInt(files_scanned) !== 1) {
+        if (parseInt(files_scanned) !== 1) {
             return `Scanned ${files_scanned} Files`;
         }
         return `Scanned ${files_scanned} Files`
     }
 
     const getLastScanTime = (index, timestamp) => {
-        if(index === 0) {
+        if (index === 0) {
             return `${formatDistanceToNow(new Date(timestamp))} ago`
         }
         return format(new Date(timestamp), "EEE dd MMM ''yy hh:mm a")
     }
 
+    // console.log(fileTimerId, timeTimerId);
     return (
         <div className="scan-section">
             {
-                slideIndex === 0 ? (    
+                slideIndex === 0 ? (
                     scanStatusData.scanning ? (
                         <div className='scan-section__active'>
                             <div className='active__title'>Quick Scan Running</div>
@@ -145,7 +164,7 @@ export default function ScanSection({data, slideIndex}) {
                                 <div className='scanner__timer'>
                                     <div className='timer__time-left'>
                                         <div className='time-left__icon'>
-                                            <img src={hourglassPng} alt='timer icon'/>
+                                            <img src={hourglassPng} alt='timer icon' />
                                         </div>
                                         <div className='time-left__value'>
                                             {+splitTimestamp(panelData.time_left)[0] ? `${splitTimestamp(panelData.time_left)[0]}:` : null}
@@ -176,11 +195,11 @@ export default function ScanSection({data, slideIndex}) {
                                 <div className='timestamp__title'>Scanned on</div>
                                 <div className='timestamp__value'>{getLastScanTime(slideIndex, timestamp)}</div>
                             </div>
-                            
+
                             <div className='inactive__scan-button'>
-                                <ScanButton startScan={() => dispatch(setScanning(true))}/>                 
+                                <ScanButton startScan={() => dispatch(setScanning(true))} />
                             </div>
-                    
+
                             <div className='inactive__scan-options' onClick={() => navigate('/user/scan-options')}>
                                 <div className='scan-options__title'>Scan Options</div>
                                 <div className='scan-options__icon'>
